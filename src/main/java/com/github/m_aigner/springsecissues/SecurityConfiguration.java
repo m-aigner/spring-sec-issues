5ef5f406e37e6774a20f3ba976e33e7652523ebd
Martin Aigner <mrtnaigner@gmail.com>
2020-06-17 21:30:55 +0200

    DnPatterns configuration with provider bean

--- src/main/java/com/github/m_aigner/springsecissues/SecurityConfiguration.java
+++ src/main/java/com/github/m_aigner/springsecissues/SecurityConfiguration.java
@@ -2,9 +2,13 @@ package com.github.m_aigner.springsecissues;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.authentication.BindAuthenticator;
+import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
+import org.springframework.security.ldap.authentication.LdapAuthenticator;
 import org.springframework.security.ldap.userdetails.UserDetailsContextMapper;
 
 @EnableWebSecurity
@@ -28,4 +32,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new LoggingUserDetailsMapper();
     }
 
+    @Bean
+    LdapAuthenticator authenticator() {
+        BindAuthenticator authenticator = new BindAuthenticator(contextSource());
+        authenticator.setUserDnPatterns(new String[]{"uid={0},ou=people"});
+        // we don't do authenticator.setUserAttributes; the default is "return all you find"
+
+        return authenticator;
+    }
+
+    @Bean
+    LdapAuthenticationProvider provider() {
+        LdapAuthenticationProvider provider = new LdapAuthenticationProvider(authenticator());
+        provider.setUserDetailsContextMapper(mapper());
+        return provider;
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder amb) {
+        amb.authenticationProvider(provider());
+    }
 }
