e737ab34a249e5c8a3c0ea4e20ba8fc186475d8c
Martin Aigner <mrtnaigner@gmail.com>
2020-06-17 18:56:31 +0200

    Fairly barebones Spring project

---
+++ src/main/java/com/github/m_aigner/springsecissues/SpringsecissuesApplication.java
@@ -0,0 +1,13 @@
+package com.github.m_aigner.springsecissues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecissuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecissuesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/m_aigner/springsecissues/SpringsecissuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.m_aigner.springsecissues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecissuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
